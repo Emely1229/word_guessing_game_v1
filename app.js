481ba091e15231de81c6cb9a5af7688b6d6dbd12
Emely Barrera <emelybarrera1229@gmail.com>
2022-04-16 16:00:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,48 @@ const phrases = [
   `National Geographic`,
   `Pretty in pink`
 ];
+
 function randomPhrase(){
   const randomPhrase = Math.floor(Math.random()*5);
   return randomPhrase;
 };
+
 function getRandomPhraseAsArray(arr) {
   var splitArray = [];
   var number = randomPhrase();
   splitArray = arr[number];
-  return splitArray
+  return splitArray.split("");
 };
-console.log(getRandomPhraseAsArray(phrases));
+var phraseArray = getRandomPhraseAsArray(phrases);
+
+
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < arr.length; i++) {
+  const item = document.createElement('li');
+  const list = document.querySelector(`#phrase ul`)
+  item.textContent = arr[i];
+  list.append(item);
+    if (arr[i].toLowerCase() >= 'a' && arr[i].toLowerCase() <= 'z') {
+      item.className = "letter";
+    }}};
+
+addPhraseToDisplay(phraseArray);
+
+let button;
+qwerty.addEventListener('click', (event)=> {
+  button = event.target
+  console.log(button.textContent);
+  button.classList = ("chosen");
+  button.disabled = true;
+});
+
+var correct = phraseArray;
+
+function checkLetter(button) {
+  for (let i = 0; i < correct.length; i++) {
+    if (correct[`${i}`].textContent === button.textContent) {
+    correct[`${i}`].classList = ("show");
+    var letterFound =+ correct[i].textContent;
+  }
+}
+}
